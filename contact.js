de9e0dd74a621a0fa2ac294893f0e0cb4d6a306e
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-05-24 22:22:51 -0300

    get contact and add contact are functional

---
+++ contact.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+const program = require('commander');
+const { addContact, getContact } = require('./logic');
+const { prompt } = require('inquirer');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'firstname',
+        message: 'Enter firstname ...'
+    },
+    {
+        type: 'input',
+        name: 'lastname',
+        message: 'Enter lastname ...'
+    },
+    {
+        type: 'input',
+        name: 'phone',
+        message: 'Enter phone number ...'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter email address ...'
+    }
+    
+]
+
+program
+    .command('addContact')
+    .alias('a')
+    .description('Add a contact')
+    .action(() => {
+        prompt(questions).then(answers => 
+        addContact(answers));
+    });
+
+program
+    .version('0.0.1')
+    .description('Contact management system')
+
+// program
+//     .command('addContact <firstname> <lastname> <phone> <email>')
+//     .alias('a')
+//     .description('Add a contact')
+//     .action((firstname, lastname, phone, email) => {
+//         addContact({firstname, lastname, phone, email});
+//     });
+
+program
+    .command('getContact <name>')
+    .alias('r')
+    .description('Get contact')
+    .action(name => getContact(name));
+
+program.parse(process.argv); 
+
---
+++ logic.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const assert = require('assert');
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connect('mongodb://localhost:27017/contact-manager');
+
+function toLower(v) {
+    return v.toLowerCase();
+}
+
+const contactSchema = mongoose.Schema({
+    firstname: { type: String, set: toLower },
+    lastname: { type: String, set: toLower },
+    phone: { type: String, set: toLower },
+    email: { type: String, set: toLower }
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+const addContact = (contact) => {
+    Contact.create(contact, (err) => {
+        assert.equal(null, err);
+        console.info('New contact added');
+        mongoose.disconnect();
+    });
+};
+
+const getContact = (name) => {
+    const search = new RegExp(name, 'i');
+    Contact.find({$or: [{firstname: search}, {lastname: search}]})
+    .exec((err, contact) => {
+        assert.equal(null, err);
+        console.info(contact);
+        console.info(`${contact.length} matches`);
+        mongoose.disconnect();
+    });
+};
+
+module.exports = { addContact, getContact }
